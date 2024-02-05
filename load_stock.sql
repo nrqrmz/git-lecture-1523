d1080f08d5e964dacbf767bc4c48e3204395659b
Enrique Ramírez Ramos <nrqrmz@gmail.com>
2024-02-05 06:39:55 -0600

    Update load_stock.sql
    Add safe_cast to change the column values to the proper data type

--- load_stock.sql
+++ load_stock.sql
@@ -1,7 +1,7 @@
 SELECT
   model_id,
-  date_creation,
-  stock,
-  price,
+  safe_cast(date_creation as date) as date_create,
+  safe_cast(stock as int64) as stock,
+  safe_cast(price as float64) as price,
 FROM
   `hip-beaker-406013.course14.circle_stock`
