83a6b784b85ad6db94277798985661a150fe3aca
Martha Huerta <marthah.112225@gmail.com>
2024-02-05 07:16:48 -0600

    Update group_by_type.sql

--- group_by_type.sql
+++ group_by_type.sql
@@ -1,8 +1,10 @@
 SELECT
-  model_type,
-  SUM(SAFE_CAST(stock AS int64)) AS stock_amount,
-  AVG(SAFE_CAST(price AS float64)) AS avg_price,
-FROM
-  `hip-beaker-406013.course14.circle_stock`
-GROUP BY
-  model_type
+    model_type,
+    color,
+    AVG(SAFE_CAST(price AS float64)) AS avg_price,
+    SUM(SAFE_CAST(stock AS int64)) AS stock_amount,
+  FROM
+    `hip-beaker-406013.course14.circle_stock`
+  GROUP BY
+    model_type,
+    color
