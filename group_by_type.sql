4ec7807bd5efb97208f5b1534db7f50aa01b5976
Enrique Ramírez Ramos <nrqrmz@gmail.com>
2024-02-05 07:26:41 -0600

    Add stock_value

--- group_by_type.sql
+++ group_by_type.sql
@@ -1,8 +1,22 @@
+WITH
+  cte AS (
+  SELECT
+    model_type,
+    color,
+    AVG(SAFE_CAST(price AS float64)) AS avg_price,
+    SUM(SAFE_CAST(stock AS int64)) AS stock_amount,
+  FROM
+    `hip-beaker-406013.course14.circle_stock`
+  GROUP BY
+    model_type,
+    color )
 SELECT
   model_type,
-  SUM(SAFE_CAST(stock AS int64)) AS stock_amount,
-  AVG(SAFE_CAST(price AS float64)) AS avg_price,
+  color,
+  avg_price,
+  stock_amount,
+  ROUND(avg_price * stock_amount,2) AS stock_value
 FROM
-  `hip-beaker-406013.course14.circle_stock`
-GROUP BY
-  model_type
+  cte
+ORDER BY
+  stock_value desc
